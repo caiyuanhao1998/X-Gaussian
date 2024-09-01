ca3ad7aeef95e57113d12dae1dbb84152e2d9bdf
Yuanhao Cai <3359145729@qq.com>
2024-09-01 13:10:02 -0400

    commit

--- submodules/diff-gaussian-rasterization/ext.cpp
+++
@@ -1,8 +0,0 @@
-#include <torch/extension.h>
-#include "rasterize_points.h"
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("rasterize_gaussians", &RasterizeGaussiansCUDA);
-  m.def("rasterize_gaussians_backward", &RasterizeGaussiansBackwardCUDA);
-  m.def("mark_visible", &markVisible);
-}
