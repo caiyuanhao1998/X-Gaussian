d3b2d88d63cacef8cca3a6d2dbd9bfd4d92d4e82
Yuanhao Cai <48472661+caiyuanhao1998@users.noreply.github.com>
2024-09-02 13:42:58 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@ python3 train.py --config config/chest.yaml --eval
 
 python3 train.py --config config/foot.yaml --eval
 
-python3 train.py --config config/abdomen.pickle --eval
+python3 train.py --config config/abdomen.yaml --eval
 
-python3 train.py --config config/head.pickle --eval
+python3 train.py --config config/head.yaml --eval
 
-python3 train.py --config config/pancreas.pickle --eval
+python3 train.py --config config/pancreas.yaml --eval
