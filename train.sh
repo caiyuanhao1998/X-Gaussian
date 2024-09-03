2f68621ac31250605af9ae07131b94ee936b6584
Yuanhao Cai <48472661+caiyuanhao1998@users.noreply.github.com>
2024-09-02 21:26:13 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -7,3 +7,24 @@ python3 train.py --config config/abdomen.yaml --eval
 python3 train.py --config config/head.yaml --eval
 
 python3 train.py --config config/pancreas.yaml --eval
+
+python3 train.py --config config/jaw.yaml --eval
+
+python3 train.py --config config/pelvis.yaml --eval
+
+python3 train.py --config config/aneurism.yaml --eval
+
+python3 train.py --config config/carp.yaml --eval
+
+python3 train.py --config config/bonsai.yaml --eval
+
+python3 train.py --config config/box.yaml --eval
+
+python3 train.py --config config/backpack.yaml --eval
+
+python3 train.py --config config/engine.yaml --eval
+
+python3 train.py --config config/leg.yaml --eval
+
+python3 train.py --config config/teapot.yaml --eval
+
